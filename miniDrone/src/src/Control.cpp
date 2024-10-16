38f13c6c290626e2356c12d5ecaae99ceab8ef3b
kn4ts <1110natsuki@gmail.com>
2024-10-16 15:05:05 +0900

    update ble send and pid gain

--- miniDrone/src/src/Control.cpp
+++ miniDrone/src/src/Control.cpp
@@ -18,20 +18,28 @@ static float ref_yaw = 0;   // ヨー目標値[degree?]
 
 /* 制御器ゲイン */
 // 高度ゲイン
+// 一番まし？
+//static float alt_Kp = 2;
+//static float alt_Ki = 2;
+//static float alt_Kd = 0.003;
+
 static float alt_Kp = 1;
 static float alt_Ki = 2;
+static float alt_Kd = 0.002;
+//static float alt_Kp = 2;
+//static float alt_Ki = 2;
 //static float alt_d = 0.05;
 //static float alt_d = 0.1;
 //static float alt_d = 0.01;
-static float alt_Kd = 0.001;
+//static float alt_Kd = 0.003;
 // ロール角度ゲイン
-static float rol_Kp = 1;
-static float rol_Ki = 1;
-static float rol_Kd = 0.001;
+static float rol_Kp = 0.2;
+static float rol_Ki = 0.01;
+static float rol_Kd = 0.01;
 // ピッチ角度ゲイン
-static float pit_Kp = 1;
-static float pit_Ki = 1;
-static float pit_Kd = 0.001;
+static float pit_Kp = 0.2;
+static float pit_Ki = 0.01;
+static float pit_Kd = 0.01;
 
 
 /* 制御器の状態変数 */
@@ -44,23 +52,26 @@ static float prev_alt = 0;  // 1ステップ前の高度
 static float rol_e = 0;
 static float rol_ei = 0;
 static float rol_ed = 0;
-static float prev_rol = 0;
+static float prev_rol_e = 0; // 1ステップ前のロール角誤差
 // ピッチ角度に関するもの
 static float pit_e = 0;
 static float pit_ei = 0;
 static float pit_ed = 0;
-static float prev_pit = 0;
+static float prev_pit_e = 0;
 // ヨー角度に関するもの
 static float yaw_e = 0;
 static float yaw_ei = 0;
 static float yaw_ed = 0;
-static float prev_yaw = 0;
+static float prev_yaw_e = 0;
+
+// 要求制御力
+static float cont_force[4] ; // 要求制御力をまとめる配列
 
 // 制御器出力
 static float uc[4]; // 制御器出力の配列
 
 // バイアス入力
-static float u_bias = 10;
+static float u_bias[4] = {10,10,15,15};
 
 // 制御器実装用の変数
 static unsigned long prevTime, currTime ; // 時刻の差分をとるための変数
@@ -68,8 +79,12 @@ static float deltaTime ; // 時刻の差分を格納する変数
 
 static int8_t status = 0 ; // ステータス
 
+
+
 /* 関数定義 */
 // 制御器の実装例
+//   引数：フィードバックされる信号
+//   返り値：PWM指令値の配列の先頭アドレス
 float* controller_demo( float* y, uint16_t alt ){
 
     // 時間算出
@@ -80,19 +95,19 @@ float* controller_demo( float* y, uint16_t alt ){
     /* 信号の更新 */ 
     // 高度情報
     alt_ed = ( -(float)alt +prev_alt ) / deltaTime ; // 微分先行で計測値を数値微分
-    alt_e = ref_alt -(float)alt ; // 誤差
+    alt_e = ref_alt -(float)alt ; // 現在の誤差
     alt_ei += alt_e * deltaTime ; // 誤差の積分
-    prev_alt = alt ; // 1ステップ前の誤差を更新
+    prev_alt = alt ; // 1ステップ前の高度を更新
     // ロール角度情報
-    rol_ed = ( -y[0] +prev_rol ) / deltaTime ; // 微分先行で計測値を数値微分
-    rol_e = ref_rol -y[1] ; // 誤差
+    rol_e = ref_rol -y[0] ; // 現在の誤差
+    rol_ed = ( rol_e -prev_rol_e ) / deltaTime ; // 誤差を数値微分
     rol_ei += rol_e * deltaTime ; // 誤差の積分
-    prev_rol = y[0] ; // 1ステップ前の誤差を更新
+    prev_rol_e = rol_e ; // 1ステップ前の誤差を更新
     // ピッチ角度情報
-    pit_ed = ( -y[1] +prev_pit ) / deltaTime ; // 微分先行で計測値を数値微分
     pit_e = ref_pit -y[1] ; // 誤差
+    pit_ed = ( pit_e -prev_pit_e ) / deltaTime ; // 微分先行で計測値を数値微分
     pit_ei += pit_e * deltaTime ; // 誤差の積分
-    prev_pit = y[1] ; // 1ステップ前の誤差を更新
+    prev_pit_e = pit_e ; // 1ステップ前の誤差を更新
 
     // ステータス（飛行状況）の判定・更新
     if( status == 0 && alt > 6 ){
@@ -108,21 +123,42 @@ float* controller_demo( float* y, uint16_t alt ){
     // ミキシング（分配）
     allocator_demo( tau_rol, tau_pit, tau_yaw, f_total );
 
+    // 変数保存&更新
+    cont_force[0] = tau_rol;
+    cont_force[1] = tau_pit;
+    cont_force[2] = tau_yaw;
+    cont_force[3] = f_total;
+
     return &uc[0];
 }
 
 // 分配器の実装例
 void allocator_demo( float t_r, float t_p, float t_y, float f_t ){
-    uc[0] = (-1) * t_r + (+1) * t_p + (+1) * t_y + (+1) * f_t + u_bias ;
-    uc[1] = (-1) * t_r + (-1) * t_p + (-1) * t_y + (+1) * f_t + u_bias ;
-    uc[2] = (+1) * t_r + (-1) * t_p + (+1) * t_y + (+1) * f_t + u_bias ;
-    uc[3] = (+1) * t_r + (+1) * t_p + (-1) * t_y + (+1) * f_t + u_bias ;
+    uc[0] = (-1) * t_r + (+1) * t_p + (+1) * t_y + (+1) * f_t + u_bias[0] ;
+    uc[1] = (-1) * t_r + (-1) * t_p + (-1) * t_y + (+1) * f_t + u_bias[1] ;
+    uc[2] = (+1) * t_r + (-1) * t_p + (+1) * t_y + (+1) * f_t + u_bias[2] ;
+    uc[3] = (+1) * t_r + (+1) * t_p + (-1) * t_y + (+1) * f_t + u_bias[3] ;
 }
+
+// 要求制御力のゲッタ関数
+float* getControlForceReq(){ return &cont_force[0]; } // 
+
+// ロール角度指令値のセッタ関数
+void setRollReference( float r ){ ref_rol = r; } // ピッチ角度指令値を更新
+// ピッチ角度指令値のセッタ関数
+void setPitchReference( float r ){ ref_pit = r; } // ピッチ角度指令値を更新
 
 // 制御器のリセット（状態変数の初期化）関数
 void initializeController(){
     alt_ei = 0;
+    rol_ei = 0;
+    pit_ei = 0;
     prev_alt = 0 ;
+    prev_rol_e = 0 ;
+    prev_pit_e = 0 ;
+    ref_rol = 0;
+    ref_pit = 0;
+    ref_yaw = 0;
     uc[0] = 0;
     uc[1] = 0;
     uc[2] = 0;
