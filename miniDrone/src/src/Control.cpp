4174b01bc3e4a71569bfba77ff63ba363427dc21
kn4ts <1110natsuki@gmail.com>
2024-10-21 21:35:37 +0900

    improve control input management

--- miniDrone/src/src/Control.cpp
+++ miniDrone/src/src/Control.cpp
@@ -24,7 +24,9 @@ static float ref_yaw = 0;   // ヨー目標値[degree?]
 // static float alt_Kd = 0.003;
 // static AltGain altK = { 2, 1, 0.001 };
 //static AltGain altK = { 1, 2, 0.0001 };
-static AltGain altK = { 1, 1, 0.0001 };
+//static AltGain altK = { 1, 1, 0.0001 };
+//static AltGain altK = { 0.5, 1, 0.0001 };
+static AltGain altK = { 0.5, 2, 0.0001 };
 //static float alt_Kp = 2;
 //static float alt_Ki = 2;
 //static float alt_d = 0.05;
@@ -32,46 +34,29 @@ static AltGain altK = { 1, 1, 0.0001 };
 //static float alt_d = 0.01;
 //static float alt_Kd = 0.003;
 // ロール角度ゲイン
-static RollGain rolK = { 0.1, 0, 0.001 };
+static RollGain rolK = { 0.2, 0.1, 0.001 };
 //static float rol_Kp = 0.2;
 //static float rol_Ki = 0.01;
 //static float rol_Kd = 0.01;
 // ピッチ角度ゲイン
-static PitchGain pitK = { 0.1, 0, 0.001 };
+static PitchGain pitK = { 0.2, 0.1, 0.001 };
 //static float pit_Kp = 0.2;
 //static float pit_Ki = 0.01;
 //static float pit_Kd = 0.01;
 
-
 /* 制御器の内部変数 */
-// 高度に関するもの
-static AltVariable alt = { 0, 0, 0, 0, 0, 0 };
-// ロール角度に関するもの
-static RollVariable rol = { 0, 0, 0, 0, 0, 0};
-// ピッチ角度に関するもの
-static PitchVariable pit = { 0, 0, 0, 0, 0, 0};
-// ヨー角度に関するもの
-static YawVariable yaw = { 0, 0, 0, 0, 0, 0};
+static AltVariable alt = { 0, 0, 0, 0, 0, 0 }; // 高度に関するもの
+static RollVariable rol = { 0, 0, 0, 0, 0, 0}; // ロール角度に関するもの
+static PitchVariable pit = { 0, 0, 0, 0, 0, 0}; // ピッチ角度に関するもの
+static YawVariable yaw = { 0, 0, 0, 0, 0, 0}; // ヨー角度に関するもの
 
 // 計測値のフィルタ処理用の変数
-//static float alt_alpha = 0.95 ; // ローパスのゲイン
-//static float alt_alpha = 0.90 ; // ローパスのゲイン
-//static float alt_alpha = 0.85 ; // ローパスのゲイン
-// static float alt_alpha = 0.80 ; // ローパスのゲイン -> おとなしくなった？ <- なってない
-// static float alt_alpha = 0.75 ; // ローパスのゲイン
-//static LowpassFilterGain alpha = { 0.7, 0.8, 0.8, 0.8 };
-//static LowpassFilterGain alpha = { 0.6, 0.8, 0.8, 0.8 };
-//static LowpassFilterGain alpha = { 0.5, 0.8, 0.8, 0.8 };
-//static LowpassFilterGain alpha = { 0.1, 0.2, 0.2, 0.2 }; // 高度かなりまし
-static LowpassFilterGain alpha = { 0.1, 0.3, 0.3, 0.3 }; // 
-//static float alt_alpha = 0.70 ; // ローパスのゲイン
+static LowpassFilterGain alpha = { 0.1, 0.3, 0.3, 0.3 }; // 高度，ロール，ピッチ，ヨーの順
 
 // 要求制御力
 static float cont_force[4] ; // 要求制御力をまとめる配列
-
 // 制御器出力
 static float uc[4]; // 制御器出力の配列
-
 // バイアス入力
 static float u_bias[4] = {10,10,15,15};
 
@@ -134,9 +119,7 @@ float* controller_demo( float* y, float distance ){
     yaw.prev = yaw.e ; // 1ステップ前の誤差を更新
 
     // ステータス（飛行状況）の判定・更新
-    if( status == 0 && distance > 6 ){
-        status = 1; // 離陸
-    }
+    if( status == 0 && distance > 6 ){ status = 1; } // 離陸
 
     // 要求制御力の計算
     float tau_rol = rolK.p * rol.e + rolK.i * rol.ei + rolK.d * rol.ed ; // ロール方向
@@ -208,12 +191,7 @@ void initializeController(){
     pit = {0, 0, 0, 0, 0, 0};
     yaw = {0, 0, 0, 0, 0, 0};
     // 目標値のリセット
-    ref_rol = 0;
-    ref_pit = 0;
-    ref_yaw = 0;
+    ref_alt = 0; ref_rol = 0; ref_pit = 0; ref_yaw = 0;
     // 制御器出力のリセット
-    uc[0] = 0;
-    uc[1] = 0;
-    uc[2] = 0;
-    uc[3] = 0;
+    uc[0] = 0; uc[1] = 0; uc[2] = 0; uc[3] = 0;
 }
