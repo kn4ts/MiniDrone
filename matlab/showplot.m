bc74455b0af716861bf168528559a7b2e6baba43
kn4ts <1110natsuki@gmail.com>
2025-02-06 10:32:51 +0900

    update showplot

--- matlab/showplot.m
+++ matlab/showplot.m
@@ -46,39 +46,56 @@ function output = showplot( depth )
 	% ===================================================================================================
 	figure()
 	% プロット作成
-	subplot( 2,2,1 )
+	subplot( 3,2,1 )
 	hold on
 	plot( time(index:end), data( index:end, colm_alti(1) ) ); % 高度
 	plot( time(index:end), data( index:end, colm_alti(2) ) ); % 高度のフィルタ値
 	plot( time(index:end), data( index:end, colm_ref(1) ), 'r' ); % 高度の目標値
 	hold off
-	legend(["altitude [mm]", "altitude (filtered) [mm]", "reference [mm]"])
+	legend(["altitude [mm]", "altitude (filtered) [mm]", "reference [mm]"], 'Location', 'best')
+	%legend('Location', 'best');  % 自動で最適な位置に凡例を配置
 	ylabel("Altitude [mm]")
 	box on, grid on
 	xlim([ time(index), time(end) ])
 	ax = gca;
 	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
 	%
-	subplot( 2,2,3 )
+	subplot( 3,2,3 )
 	hold on
-	plot( time(index:end), data( index:end, colm_atti(1):colm_atti(2) ) ); % 姿勢角
-	plot( time(index:end), data( index:end, colm_atti_f(1):colm_atti_f(2) ), '--' ); % 姿勢角のフィルタ値
+	plot( time(index:end), data( index:end, colm_atti(1):colm_atti(2)-1 ) ); % 姿勢角（ロール，ピッチ）
+	plot( time(index:end), data( index:end, colm_atti_f(1):colm_atti_f(2)-1 ), '--' ); % 姿勢角のフィルタ値
 	plot( time(index:end), data( index:end, colm_ref(2)-1:colm_ref(2) ), 'r:' ); % 姿勢角の目標値
 	hold off
-	legend(["roll [deg]", "pitch [deg]", "yaw [deg]", ...
-	        "roll(filtered) [deg]", "pitch(filtered) [deg]", "yaw(filtered) [deg]", ...
-			"reference(roll) [deg]", "reference(pitch) [deg]"])
+	legend(["roll [deg]", "pitch [deg]", ...
+	        "roll(filtered) [deg]", "pitch(filtered) [deg]", ...
+			"reference(roll) [deg]", "reference(pitch) [deg]"], 'Location', 'best')
 	box on, grid on
 	xlim([ time(index), time(end) ])
-	ylabel("Attitude [deg]")
+	ylabel("Attitude (Roll, Pitch) [deg]")
 	xlabel("Time (in FC) [s]")
 	ax = gca;
 	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
 	%plot( data(:,colm_time), data(:,colm_alti(1):colm_alti(2)) )
 	%
+	subplot( 3,2,5 )
+	hold on
+	plot( time(index:end), data( index:end, colm_atti(2) ) ); % 姿勢角（ヨー）
+	plot( time(index:end), data( index:end, colm_atti_f(2) ), '--' ); % 姿勢角のフィルタ値
+	%plot( time(index:end), data( index:end, colm_ref(2)-1:colm_ref(2) ), 'r:' ); % 姿勢角の目標値
+	hold off
+	legend(["yaw [deg]", ...
+	        "yaw(filtered) [deg]"] , 'Location', 'best')
+			%"reference(roll) [deg]", "reference(pitch) [deg]"])
+	box on, grid on
+	xlim([ time(index), time(end) ])
+	ylabel("Attitude (Yaw) [deg]")
+	xlabel("Time (in FC) [s]")
+	ax = gca;
+	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
+	%
 	subplot( 2,2,2 )
 	plot( time(index:end), data( index:end, colm_uc(1):colm_uc(2) ) ); % 高度
-	legend(["uc_1", "uc_2", "uc_3", "uc_4"])
+	legend(["uc_1", "uc_2", "uc_3", "uc_4"], 'Location', 'best')
 	ylabel("Controller output [-]")
 	box on, grid on
 	xlim([ time(index), time(end) ])
@@ -87,7 +104,7 @@ function output = showplot( depth )
 	%
 	subplot( 2,2,4 )
 	plot( time(index:end), data( index:end, colm_cf(1):colm_cf(2) ) ); % 姿勢角
-	legend(["tau_roll", "tau_pitch", "tau_yaw", "f_all"])
+	legend(["tau_roll", "tau_pitch", "tau_yaw", "f_all"], 'Location', 'best')
 	box on, grid on
 	xlim([ time(index), time(end) ])
 	ylabel("Control force [-]")
@@ -98,7 +115,7 @@ function output = showplot( depth )
 	figure()
 	subplot(2,1,1)
 	plot( time(index:end), data( index:end, end-1:end ) ); % 時刻
-	legend(["in PC time", "in FC time"])
+	legend(["in PC time", "in FC time"], 'Location', 'best')
 	box on, grid on
 	xlim([ time(index), time(end) ])
 	ylabel("difference time [ms]")
@@ -107,7 +124,7 @@ function output = showplot( depth )
 	%
 	subplot(2,1,2)
 	plot( time(index:end), data( index:end, end-1:end ) ); % 時刻
-	legend(["in PC time", "in FC time"])
+	legend(["in PC time", "in FC time"], 'Location', 'best')
 	box on, grid on
 	xlim([ time(index), time(end) ])
 	ylim([ -1, 4 * max( median( data(index:end, end-1)), median( data(index:end, end))) ])
@@ -116,6 +133,86 @@ function output = showplot( depth )
 	ax = gca;
 	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
 
+	%
+	figure()
+	% プロット作成
+	subplot( 2,3,1 )
+	hold on
+	plot( time(index:end), data( index:end, colm_alti(1) ) ); % 高度
+	plot( time(index:end), data( index:end, colm_alti(2) ) ); % 高度のフィルタ値
+	plot( time(index:end), data( index:end, colm_ref(1) ), 'r' ); % 高度の目標値
+	hold off
+	legend(["altitude [mm]", "altitude (filtered) [mm]", "reference [mm]"], 'Location', 'best')
+	ylabel("Altitude [mm]")
+	box on, grid on
+	xlim([ time(index), time(end) ])
+	ax = gca;
+	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
+	%
+	subplot( 2,3,4 )
+	plot( time(index:end), data( index:end, colm_cf(1)+3 ) ); % 総推力
+	legend(["f_all"], 'Location', 'best')
+	box on, grid on
+	xlim([ time(index), time(end) ])
+	ylabel("Control force (F_all) [-]")
+	xlabel("Time (in FC) [s]")
+	ax = gca;
+	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
+	%
+	subplot( 2,3,2 )
+	hold on
+	plot( time(index:end), data( index:end, colm_atti(1):colm_atti(2)-1 ) ); % 姿勢角（ロール，ピッチ）
+	plot( time(index:end), data( index:end, colm_atti_f(1):colm_atti_f(2)-1 ), '--' ); % 姿勢角のフィルタ値
+	plot( time(index:end), data( index:end, colm_ref(2)-1:colm_ref(2) ), 'r:' ); % 姿勢角の目標値
+	hold off
+	legend(["roll [deg]", "pitch [deg]", ...
+	        "roll(filtered) [deg]", "pitch(filtered) [deg]", ...
+			"reference(roll) [deg]", "reference(pitch) [deg]"], 'Location', 'best')
+	box on, grid on
+	xlim([ time(index), time(end) ])
+	ylabel("Attitude (Roll, Pitch) [deg]")
+	xlabel("Time (in FC) [s]")
+	ax = gca;
+	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
+	%
+	subplot( 2,3,5 )
+	plot( time(index:end), data( index:end, colm_cf(1):colm_cf(1)+1 ) ); % 制御力（ロール，ピッチまわり）
+	legend(["tau_roll", "tau_pitch"], 'Location', 'best')
+	box on, grid on
+	xlim([ time(index), time(end) ])
+	ylabel("Control force (tau_roll, tau_pitch) [-]")
+	xlabel("Time (in FC) [s]")
+	ax = gca;
+	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
+	%plot( data(:,colm_time), data(:,colm_alti(1):colm_alti(2)) )
+	%
+	subplot( 2,3,3 )
+	hold on
+	plot( time(index:end), data( index:end, colm_atti(2) ) ); % 姿勢角（ヨー）
+	plot( time(index:end), data( index:end, colm_atti_f(2) ), '--' ); % 姿勢角のフィルタ値
+	%plot( time(index:end), data( index:end, colm_ref(2)-1:colm_ref(2) ), 'r:' ); % 姿勢角の目標値
+	hold off
+	legend(["yaw [deg]", ...
+	        "yaw(filtered) [deg]"] , 'Location', 'best')
+			%"reference(roll) [deg]", "reference(pitch) [deg]"])
+	box on, grid on
+	xlim([ time(index), time(end) ])
+	ylabel("Attitude (Yaw) [deg]")
+	xlabel("Time (in FC) [s]")
+	ax = gca;
+	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
+	%
+	subplot( 2,3,6 )
+	plot( time(index:end), data( index:end, colm_cf(1)+2 ) ); % ヨー方向制御力
+	legend(["tau_yaw"], 'Location', 'best')
+	box on, grid on
+	xlim([ time(index), time(end) ])
+	ylabel("Control force (tau_yaw) [-]")
+	xlabel("Time (in FC) [s]")
+	ax = gca;
+	ax.XAxis.Exponent = 0;  % X軸の指数表示を無効に
+	%plot( data(:,colm_time), data(:,colm_alti(1):colm_alti(2)) )
+	%
 
 	%output = data;
 	%output = data(:,colm_time);
