e80284d27605d021b5112cb29e2d6b73114bf865
kn4ts <1110natsuki@gmail.com>
2025-02-07 18:05:45 +0900

    implement test mode (micon)

--- miniDrone/miniDrone.ino
+++ miniDrone/miniDrone.ino
@@ -90,6 +90,9 @@ void modeDetectionBLE(){
         mode = 10; // モードを10に変更
         setAltitudeReference(100); // 高度目標値をセット
         break;
+      case 't': // ロール・ピッチ角制御動作の確認モード
+        mode = 20; //
+        break;
       case 'c': // 受信文字が（char型の）'c'なら
         calibrateSensors();     // センサのバイアス値設定
         initializeController(); // 制御器をリセット
@@ -268,6 +271,21 @@ void loop() {
           uc[1] = uc_pointer[1];
           uc[2] = uc_pointer[2];
           uc[3] = uc_pointer[3];
+          break;
+        case 20:
+          if(0<att[0]){ // ロール角が正なら
+            uc[0] = 20 ; uc[1] = 20 ; uc[2] = 0 ; uc[3] = 0 ;
+          }else if(0<att[0]){ // ロール角が負なら
+            uc[0] = 0 ; uc[1] = 0 ; uc[2] = 20 ; uc[3] = 20 ;
+          }else{
+            uc[0] = 0 ; uc[1] = 0 ; uc[2] = 0 ; uc[3] = 0 ;
+          }
+          if(0<att[1]){ // ピッチ角が正なら
+            uc[0] = 0 ; uc[1] += 20 ; uc[2] += 20 ; uc[3] = 0 ;
+          }else if(0>att[1]){ // ピッチ角が負なら
+            uc[0] += 20 ; uc[1] = 0 ; uc[2] = 0 ; uc[3] += 20 ;
+          }else{};
+
           break;
         default: // mode のデフォルト設定
           uc_pointer = setUc( 0, 0, 0, 0 ); break; // 全入力を0に
